fbeb14db448f6530f094fe40a7322597d39ea4c9
andrei-grigo <andrei.grigo89@gmail.com>
2025-03-15 15:51:26 +0200

    reupload

---
+++ Program/Produs.cs
@@ -0,0 +1,28 @@
+﻿namespace Proiect01
+{
+    public class Produs
+    {
+        public string Denumire;
+        public int Cantitate;
+        public bool EsteCumparat;
+
+        public Produs(string denumire, int cantitate)
+        {
+            Denumire = denumire;
+            EsteCumparat = false;
+            Cantitate = cantitate;
+        }
+
+        public string GetData()
+        {
+            string status = "De cumparat";
+
+            if (EsteCumparat)
+            {
+                status = "Achizitionat";
+            }
+
+            return $"{Denumire} - {Cantitate}buc. - {status}";
+        }
+    }
+}
